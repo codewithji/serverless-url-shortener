5daf8e413f6716647f91a3070ffac0b1255effb5
Ji Shin <fullstack.jii@gmail.com>
2023-09-26 16:27:49 -0400

    set up initial React/TypeScript frontend with Vite

---
+++ frontend/src/components/buttons/PrimaryButton/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import styles from "./primaryButton.module.scss";
+
+interface Props {
+  text: string;
+  handleClick: () => Promise<void>;
+}
+
+const PrimaryButton: FC<Props> = ({ text, handleClick }) => {
+  return (
+    <button className={styles.primary} onClick={handleClick}>
+      {text}
+    </button>
+  );
+};
+
+export default PrimaryButton;
